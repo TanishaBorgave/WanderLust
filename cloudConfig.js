de3e174e862fd07fefe000868478c892ca36d3cd
TanishaBorgave <tanishaborgave1@gmail.com>
2026-07-25 12:21:04 +0530

    cloud

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,22 +1,21 @@
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET
+    api_secret: process.env.CLOUD_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
-    cloudinary : cloudinary,
+    cloudinary,
     params: {
-        folder: 'Wanderlust_DEV',
-        allowed_Formats: ['jpeg', 'png', 'jpg']
+        folder: "Wanderlust_DEV",
+        allowedFormats: ["jpeg", "png", "jpg"],
     },
 });
 
 module.exports = {
     cloudinary,
-    storage
-}
-
+    storage,
+};
